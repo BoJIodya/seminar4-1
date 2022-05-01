96e5664683a8edc8419b56b7a160f073e034e5c9
Vladimir Loginov <BoJIodya@yandex.ru>
2022-05-01 22:23:32 +0300

    корекция условий задачи

--- Мидл5/Program.cs
+++ Мидл5/Program.cs
@@ -1,14 +1,14 @@
-﻿int a = 12312;
+﻿int a = 98547254;
 //int result = (int)Math.Log10(a) + 1; //просто считает кол-во цифр
 
-int Method(int a) // считает сколько цифр в числе и произведение, через остаток от деления
+int Method(int a) // считает сколько цифр в числе и их сумму, через остаток от деления
 {
     int count = 0;
     int result = a;
-    int proizveden = 1;
-    while (result > 0) // если в числе будет 0, то произведение будет = 0, не зависимо от кол-ва цифр 
+    int proizveden = 0;
+    while (result > 0) 
     {
-        proizveden = proizveden * (result % 10);
+        proizveden = proizveden + (result % 10); // переменная так названа, потому что сначала считал произведение, но суть та же
         result = result / 10;
         count++;
     }
