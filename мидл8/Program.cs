72f76cf5ace23254365913e8748b5006b82e388d
Vladimir Loginov <BoJIodya@yandex.ru>
2022-05-03 18:56:27 +0300

    programm is working

--- мидл8/Program.cs
+++ мидл8/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void RandomArray(int[] array) //заполняем массив
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(0, 100);
+    }
+
+}
+void coub(int[] array) //возводим в куб каждое число массива и проверяем, четность последнего числа
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        int a = (int)Math.Pow(array[i], 3); // возводим в куб
+        if (a % 2 == 0)  // если число четное, то последняя цифра так же четная
+        {
+            Console.WriteLine($"{a}, четное");
+        }
+        else
+            Console.WriteLine($"{a}, нечетное");
+
+    }
+}
+
+int[] array = new int[5];
+RandomArray(array);
+coub(array);
